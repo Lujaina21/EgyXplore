76ced360b00db17ba4e1e2a28c2807ec38ad6ea6
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2024-03-11 01:57:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use restrict";
+const productContainers = [...document.querySelectorAll('.recommended-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
