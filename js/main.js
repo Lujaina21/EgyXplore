0602ec48d1aa05f0d40c07a5cc0bd63dd72bd3ca
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2024-04-26 06:17:53 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,15 +7,13 @@ const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
 
 productContainers.forEach((item, i) => {
-  let containerDimensions = item.getBoundingClientRect();
-  let containerWidth = containerDimensions.width;
-
+  
   nxtBtn[i].addEventListener("click", () => {
-    item.scrollLeft += containerWidth;
+    item.scrollLeft += 382;
   });
 
   preBtn[i].addEventListener("click", () => {
-    item.scrollLeft -= containerWidth;
+    item.scrollLeft -= 382;
   });
 });
 
