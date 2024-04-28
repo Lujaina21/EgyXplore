0ccc90572b18d9650ab65007cee771e2c7872876
Lujaina Walid <90357532+Lujaina21@users.noreply.github.com>
2024-04-28 04:39:37 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
 const preBtn = [...document.querySelectorAll(".pre-btn")];
 
 recommendContainer.forEach((item, i) => {
-
   nxtBtn[i].addEventListener("click", () => {
     item.scrollLeft += 382;
   });
@@ -28,3 +27,35 @@ toggleBtn.onclick = function () {
 
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
+
+/*================ Pop-Up in reviews section ===============*/
+document.addEventListener("DOMContentLoaded", function () {
+  const popupOverlay = document.getElementById("popupOverlay");
+  const popup = document.getElementById("popup");
+  const closePopup = document.getElementById("closePopup");
+  const emailInput = document.getElementById("emailInput");
+  // open fn
+  function openPopup() {
+    popupOverlay.style.display = "block";
+  }
+  // close fn
+  function cancelForm() {
+    popupOverlay.style.display = "none";
+  }
+  // send fn
+  function sendForm() {
+    const email = emailInput.value;
+    console.log(`Your review sent successfully!`);
+    cancelForm();
+  }
+  // Trigger the popup to open (you can call this function on a btn click or any event)
+  openPopup();
+  closePopup.addEventListener("click", cancelForm);
+
+  // close when clicking outside the popup
+  popupOverlay.addEventListener("click", function (event) {
+    if (event.target === popupOverlay) {
+      cancelForm();
+    }
+  });
+});
